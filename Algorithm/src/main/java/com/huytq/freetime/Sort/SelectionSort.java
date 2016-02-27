ddd680c77fe329cf94a5cbdbd3345869addfd086
Tran-Quang, Huy <tqh@automic.com>
2016-02-27 11:20:58 +0700

    initation algorithm branch

---
+++ Algorithm/src/main/java/com/huytq/freetime/Sort/SelectionSort.java
@@ -0,0 +1,22 @@
+package com.huytq.freetime.Sort;
+
+/**
+ * Created by tqh on 2/27/2016.
+ */
+public class SelectionSort {
+
+    public static int[] selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++)
+                if (arr[j] < arr[index])
+                    index = j;
+
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+        return arr;
+    }
+}
---
+++ Algorithm/src/main/java/com/huytq/freetime/Tree/BTreeNode.java
@@ -0,0 +1,7 @@
+package com.huytq.freetime.Tree;
+
+/**
+ * Created by tqh on 2/27/2016.
+ */
+public class BTreeNode {
+}
